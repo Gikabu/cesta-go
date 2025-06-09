c506dd1af88a8c783be41819d633b738dbb7e92b
gikabu <gikabuh@gmail.com>
2025-06-10 01:46:08 +0300

    cdrive

--- cesta/url.go
+++ cesta/url.go
@@ -1,8 +1,16 @@
 package cesta
 
-import "net/url"
+import (
+	"net/url"
+	"regexp"
+)
 
 func IsValidUrl(str string) bool {
 	u, err := url.Parse(str)
 	return err == nil && u.Scheme != "" && u.Host != ""
+}
+
+func IsValidHostname(hostname string) bool {
+	var hostnameRegex = regexp.MustCompile(`^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)*([a-zA-Z]{2,})$`)
+	return hostnameRegex.MatchString(hostname)
 }
